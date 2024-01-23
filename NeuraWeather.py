a5e690a8107f475ab94641f97034b5fbe809402e
CHEGEBB <chegephil24@gmail.com>
2024-01-23 15:25:25 +0300

    UPDATE

--- NeuraWeather.py
+++ NeuraWeather.py
@@ -3,6 +3,8 @@ from rich.console import Console
 from rich.progress import Progress
 from rich.prompt import Prompt
 import time
+import os
+
 
 console = Console()
 
@@ -52,15 +54,11 @@ def welcome_message():
     console.print("Temperature will be displayed in Celsius.")
 
 def main():
-    api_key = "aaf06191d54d5d7adcf736c7380775ce"  # Replace with your actual API key
-
+    api_key = os.getenv("WEATHER_API_KEY")  
     welcome_message()
-
     city_name = Prompt.ask('Enter the city name: ')
-
     display_loading_animation()
     weather_data = get_weather(api_key, city_name)
-
     display_weather(weather_data, "metric")
 
 if __name__ == "__main__":
